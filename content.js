7213cb176f5f7947ff3af6cd49526c66f6c24f46
BeffJetten <37084504+BeffJetten@users.noreply.github.com>
2018-03-26 23:12:23 -0400

    updated regex logic
    Fixed a logic bug where text replaced for all mentions of the first name. The plugin will now only replace the text if the last name is found somewhere on the page.

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-var elements = document.getElementsByTagName('*');
 
+var elements = document.getElementsByTagName('*');
+var lastNameSeen; //variable to handle more complicated regex logic
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -8,9 +9,12 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
+            if (text.includes("Betten") || text.includes("betten")){
+               lastNameSeen = true;
+            }
             var replacedText = text.replace(/Jeff Betten|Jeff|Betten/i, 'The straight up pussy bitch');
 
-            if (replacedText !== text) {
+            if (replacedText !== text && lastNameSeen == true) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
